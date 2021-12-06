c3bfe19b16e34d8fe019be647a0f06e01b8cdbfc
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2021-12-06 19:47:45 +0200

    Created rangedweaponClasses

---
+++ GoblinHunterFinal2/RangedWeapons.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoblinHunterFinal2
+{
+    class RangedWeapons : Weapon
+    {
+        public RangedWeapons(weaponType _WT,int _X, int _Y, string _Symbol) : base(_X, _Y, _Symbol)
+        {
+            String WeaponType = _Symbol;
+            WeaponType = "Rifle";
+            WeaponType = "Longbow";
+            RangedWeapons RRWPN = new RangedWeapons(weaponType.Rifle, 0, 0, "R");
+            RangedWeapons RBWPN = new RangedWeapons(weaponType.Longbow, 0, 0, "LB");
+
+
+            RRWPN.Durability = 3;
+            RRWPN.Range = 3;
+            RRWPN.Damage = 5;
+            RRWPN.Cost = 7;
+
+            RBWPN.Durability = 4;
+            RBWPN.Range = 2;
+            RBWPN.Damage = 4;
+            RBWPN.Cost = 6;
+        }
+
+        public enum weaponType
+        {
+            Rifle,
+            Longbow
+        }
+
+        private weaponType wt;
+
+        public weaponType WT
+        {
+            get { return wt; }
+            set { wt = value; }
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
