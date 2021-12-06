acaccff58c61720375bef43e3c0d74d3e7bd5746
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2021-12-06 19:30:11 +0200

    got the meleeweapons class

--- GoblinHunterFinal2/MeleeWeapon.cs
+++ GoblinHunterFinal2/MeleeWeapon.cs
@@ -6,10 +6,26 @@ namespace GoblinHunterFinal2
 {
     class MeleeWeapon : Weapon
     {
+        
+
+        
         public MeleeWeapon(weaponType _WT, int _X, int _Y, string _Symbol) : base(_X, _Y, _Symbol)
         {
             String WeaponType = _Symbol;
             WeaponType = "Dagger";
+            WeaponType = "LongSord";
+            MeleeWeapon MDWPN = new MeleeWeapon(weaponType.Dagger,0,0,"D");
+            MeleeWeapon MLWPN = new MeleeWeapon(weaponType.LongSword, 0, 0, "LS");
+
+
+            MDWPN.Durability = 10;
+            MDWPN.Damage = 3;
+            MDWPN.Cost = 3;
+
+            MLWPN.Durability = 6;
+            MLWPN.Damage = 4;
+            MLWPN.Cost = 5;
+
 
         }
 
